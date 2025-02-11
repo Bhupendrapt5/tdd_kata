d302bbb9d787cccb5726b4bd4fb9f775945456a6
M0rph3us <bhupendrapt95@gmail.com>
2025-02-11 20:03:19 +0530

    first commit

---
+++ bin/tdd_kata_assesment.dart
@@ -0,0 +1,6 @@
+import 'package:tdd_kata_assesment/tdd_kata_assesment.dart'
+    as tdd_kata_assesment;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tdd_kata_assesment.calculate()}!');
+}
---
+++ test/tdd_kata_assesment_test.dart
@@ -0,0 +1,8 @@
+import 'package:tdd_kata_assesment/tdd_kata_assesment.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
